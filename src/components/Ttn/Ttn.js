9eb850c13d1a9d63c16f66327908288bff8c45e4
Victoriia-Ripka <vikaripka@gmail.com>
2023-03-09 21:45:22 +0100

    styles for formik

--- src/components/Ttn/Ttn.js
+++ src/components/Ttn/Ttn.js
@@ -1,15 +1,16 @@
 import React from "react";
-import { ErrorMessage, Field, Form, Formik, useFormik } from "formik";
+import { useFormik } from "formik";
 import { styled } from "@mui/material/styles";
 import * as yup from "yup";
-import Container from "@mui/material/Container";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
+import Button from '@mui/material/Button';
+import TextField from "@mui/material/TextField";
 import { Sidebar } from "react-pro-sidebar";
 
-const validationSchema = yup.object({
-  TTN: yup.string("Enter your TTN number").required("number is required"),
-});
+const ttnValidationSchema = yup
+  .string("Enter your TTN number")
+  .required("number is required");
 
 const Root = styled("div")(({ theme }) => ({
   padding: theme.spacing(1),
@@ -47,28 +48,36 @@ const RootSidebar = styled("div")(({ theme }) => ({
 }));
 
 const Ttn = () => {
-  const initialState = {
-    TTN: "",
-  };
+  const formik = useFormik({
+    initialValues: {
+      TTN: "",
+    },
+    validationSchema: ttnValidationSchema,
+    onSubmit: async (values) => {
+      await new Promise((r) => setTimeout(r, 500));
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
 
   return (
     <Root>
-      <Box sx={{ pr: "30px" }}>
+      <Box sx={{ mr: "30px" }}>
         <Box>
-          <Formik
-            initialValues={initialState}
-            onSubmit={async (values) => {
-              await new Promise((r) => setTimeout(r, 500));
-              alert(JSON.stringify(values, null, 2));
-            }}
-          >
-            <Form>
-              <Field name="TTN" placeholder="TTN number" />
-              <ErrorMessage name="TTN" component="p" />
-
-              <button type="submit">Get Status TTN</button>
-            </Form>
-          </Formik>
+          <form onSubmit={formik.handleSubmit}>
+            <TextField
+              fullWidth
+              id="TTN"
+              name="TTN"
+              label="TTN"
+              value={formik.values.TTN}
+              onChange={formik.handleChange}
+              error={formik.touched.email && Boolean(formik.errors.email)}
+              helperText={formik.touched.email && formik.errors.email}
+            ></TextField>
+            <Button color="primary" variant="contained" fullWidth type="submit" sx={{mt: "10px"}}>
+              Get Status TTN
+            </Button>
+          </form>
         </Box>
         <Box
           sx={{
